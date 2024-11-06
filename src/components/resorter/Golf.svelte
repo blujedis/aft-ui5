11707e04783f82f449b7270311be4c004e324dd9
blujedis <blujedicorp@gmail.com>
2024-11-06 11:36:12 -0800

    chore:ptw

---
+++ src/components/resorter/Golf.svelte
@@ -0,0 +1,177 @@
+<div class="max-w-4xl px-5 md:px-0 mt-8">
+	<div class="md:grid md:grid-cols-1 md:gap-x-8 mb-12">
+		<div class="pb-4">
+			<img
+				alt=""
+				class="w-full"
+				data-testid="image-card-populated"
+				src="https://resorter.ghost.io/content/images/2024/09/Course_Layout.jpg"
+			/>
+		</div>
+
+		<div class="flex flex-col">
+			<div class="mb-4">
+				<h4 class="sm:!mt-0">Green Fees</h4>
+				<div class="grid grid-cols-2 gap-x-4 gap-y-2">
+					<div>Nov-Jan 9 (Open to 7:59 am)</div>
+					<div>
+						<div class="font-bold">$80 (Sun-Thu)</div>
+						<div class="font-bold">$85 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (8 am to 11:59 am)</div>
+					<div>
+						<div class="font-bold">$105 (Sun-Thu)</div>
+						<div class="font-bold">$115 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (12 pm to 1:59 pm)</div>
+					<div>
+						<div class="font-bold">$80 (Sun-Thu)</div>
+						<div class="font-bold">$85 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (After 2 pm)</div>
+					<div>
+						<div class="font-bold">$50 (Sun-Thu)</div>
+						<div class="font-bold">$55 (Fri-Sat)</div>
+					</div>
+
+					<div>Jan 10-Mar (Open to 7:59 am)</div>
+					<div>
+						<div class="font-bold">$85 (Sun-Thu)</div>
+						<div class="font-bold">$90 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (8 am to 11:59 am)</div>
+					<div>
+						<div class="font-bold">$115 (Sun-Thu)</div>
+						<div class="font-bold">$120 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (12 pm to 1:59 pm)</div>
+					<div>
+						<div class="font-bold">$85 (Sun-Thu)</div>
+						<div class="font-bold">$90 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (After 2 pm)</div>
+					<div>
+						<div class="font-bold">$55 (Sun-Thu)</div>
+						<div class="font-bold">$60 (Fri-Sat)</div>
+					</div>
+
+					<div>Apr 2025 (Open to 7:59 am)</div>
+					<div>
+						<div class="font-bold">$60 (Sun-Thu)</div>
+						<div class="font-bold">$65 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (8 am to 11:59 am)</div>
+					<div>
+						<div class="font-bold">$80 (Sun-Thu)</div>
+						<div class="font-bold">$85 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (12 pm to 2:59 pm)</div>
+					<div>
+						<div class="font-bold">$60 (Sun-Thu)</div>
+						<div class="font-bold">$65 (Fri-Sat)</div>
+					</div>
+
+					<div>Nov-Jan 9 (After 3 pm)</div>
+					<div>
+						<div class="font-bold">$40 (Sun-Thu)</div>
+						<div class="font-bold">$45 (Fri-Sat)</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="mb-4">
+				<h4 class="sm:!mt-0">Other Fees</h4>
+				<div class="grid grid-cols-2 gap-x-4 gap-y-2">
+					<div>Club Rental (Srixon/Cleveland)</div>
+					<div>
+						<div class="font-bold">$49 Nov-Dec</div>
+						<div class="font-bold">$59 Jan-Mar</div>
+						<div class="font-bold">$49 Apr</div>
+					</div>
+					<div>Driving Range Public</div>
+					<div>
+						<div class="font-bold">$15 <span class="text-sm">hr</span> (daily)</div>
+						<div class="font-bold">$200 (monthly)</div>
+					</div>
+					<!-- <div>Driving Range Monthly</div>
+					<div class="font-bold">$150</div> -->
+					<div>Golf Instruction</div>
+					<div class="font-bold">$75 <span class="text-sm">1/2 hr</span></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<hr />
+
+	<div class="mb-12">
+		<h4>Contact Golf Shop</h4>
+
+		<div class="md:grid md:grid-cols-[15%_85%] md:gap-x-4 mb-4">
+			<div class="font-medium pr-3">Phone:</div>
+			<div><a href="tel:7603452791">760.345.2791</a></div>
+
+			<div class="font-medium pr-3">Conditions:</div>
+			<div>
+				<a href="https://www.theweathernetwork.com/us/weather/california/palm-desert/92260"
+					>Weather Network</a
+				>
+			</div>
+		</div>
+	</div>
+
+	<div class="mb-12">
+		<h4>Hours of Operation</h4>
+
+		<div class="md:grid md:grid-cols-[15%_85%] md:gap-x-4 mb-4">
+			<div class="font-medium pr-3">Golf Shop:</div>
+			<div>6 AM to Noon Daily</div>
+
+			<div class="font-medium pr-3">Driving Range:</div>
+			<div>6:30 AM to 4:30 PM</div>
+
+			<div class="font-medium pr-3">Driving Range Closed:</div>
+			<div>Wednesdays 10:00 AM to 1:00 PM</div>
+		</div>
+	</div>
+
+	<hr />
+
+	<div class="mb-12">
+		<h4>Dress Code</h4>
+
+		<div class="md:grid md:grid-cols-[15%_85%] md:gap-x-4 mb-4">
+			<div class="font-medium pr-3">Men:</div>
+			<div>
+				Collared shirts or mock turtleneck shirts. Slacks or bermuda length shorts are permitted.
+			</div>
+
+			<div class="font-medium pr-3">Women:</div>
+			<div>
+				Collared golf shirts with or without sleeves. Slacks or bermuda length shorts, skorts and
+				skirts are permitted.
+			</div>
+
+			<div class="font-medium pr-3">Prohibited:</div>
+			<div>Jeans, athletic clothing, swimwear, t-shorts or tank tops.</div>
+		</div>
+	</div>
+
+	<hr />
+
+	<div class="mb-12">
+		<h5>Disclaimers</h5>
+		<!-- <p class="text-sm">
+			Currently 16 holes are available. Holes 9 and 18 are closed for lake maintenance.
+		</p> -->
+		<p class="text-sm text-amber-600">Prices subject to change without notice</p>
+	</div>
+</div>
---
+++ src/components/resorter/GolfMembership.svelte
@@ -0,0 +1,210 @@
+<div class="max-w-4xl px-5 md:px-0">
+
+
+
+  <div class="md:grid md:grid-cols-2 gap-x-12 mb-14">
+    
+    <div>
+      <h4 class="sm:!mt-0">Full Golf Membership</h4>
+      <p>
+        • Unlimited golf through active season<br/>
+        • Unlimited practice range use<br/>
+        • Eighteen-day advance booking<br/>
+        <br/>
+        • Exclusive member only golf & social events<br/>
+        • 10% off food and beverage & golf shop soft goods<br/>
+        • $550 F&B minimum for Non Homeowner</br>
+        • Guests 10% off best public rate for date & time
+      </p>
+    </div>
+
+
+    <div>
+      <h4 class="sm:!mt-0">Gold Membership Fees</h4>
+      <div class="grid grid-cols-2 gap-x-4">
+          <div>Homeowner Single</div>
+          <div class="font-bold">$5,750</div>
+          <div>Homeowner Couple</div>
+          <div class="font-bold">$7,175</div>
+          <div>Non-Homeowner Single</div>
+          <div class="font-bold">$6,200</div>
+          <div>Non-Homeowner Couple</div>
+          <div class="font-bold">$7,800</div>
+          <div>Non-Homeowner Couple</div>
+          <div class="font-bold">$7,800</div>
+      </div>
+    </div>
+
+    
+ 
+  </div>
+
+  <hr/>
+
+  <div class="md:grid md:grid-cols-2 gap-x-12 mb-14">
+    
+    <div>
+      <h4 class="sm:!mt-0">Monthly Membership</h4>
+      <p>
+        • Includes up to 18 rounds (single) with 34 rounds (per couple) per month.<br/>
+        • Additional rounds $70.00 per round<br/>
+        • Months do NOT have to be consecutive</br/>
+        • Unlimited practice range use</br/>
+        • Fourteen-day advance booking</br/>
+        • Children (under 25 years of age) included in the membership</br/>
+        • Exclusive member only golf & social events</br/>
+        • 10% off food and beverage & golf shop soft goods</br/>
+        • $75 /Month F&B minimum for Non-Homeowners</br/>
+        • Must be purchased in advance</br/>
+        • Personal cart trail fee – $75/month</br/>
+        • Guests 10% off seasonal public rate based on date & time</br/>
+      </p>
+    </div>
+
+
+    <div>
+      <h4 class="sm:!mt-0">Monthly Membership Fees</h4>
+
+      <p class="text-amber-700 mb-3">1 Month</p>
+      <div class="grid grid-cols-2 gap-x-4">
+          <div>Homeowner Single</div>
+          <div class="font-bold">$1,360</div>
+          <div>Homeowner Couple</div>
+          <div class="font-bold">$2,580</div>
+          <div>Non-Homeowner Single</div>
+          <div class="font-bold">$1,460</div>
+          <div>Non-Homeowner Couple</div>
+          <div class="font-bold">$2,760</div>
+      </div>
+
+      <p class="text-amber-700 mb-3 mt-3">2 Month</p>
+      <div class="grid grid-cols-2 gap-x-4">
+        <div>Homeowner Single</div>
+        <div class="font-bold">$2,580</div>
+        <div>Homeowner Couple</div>
+        <div class="font-bold">$4,870</div>
+        <div>Non-Homeowner Single</div>
+        <div class="font-bold">$2,770</div>
+        <div>Non-Homeowner Couple</div>
+        <div class="font-bold">$5,220</div>
+      </div>
+
+      <p class="text-amber-700 mb-3 mt-3">3 Month</p>
+      <div class="grid grid-cols-2 gap-x-4">
+        <div>Homeowner Single</div>
+        <div class="font-bold">$3,590</div>
+        <div>Homeowner Couple</div>
+        <div class="font-bold">$6,800</div>
+        <div>Non-Homeowner Single</div>
+        <div class="font-bold">$3,870</div>
+        <div>Non-Homeowner Couple</div>
+        <div class="font-bold">$7,300</div>
+      </div>
+
+    </div>
+
+    
+  </div>
+
+  <hr/>
+
+  <!-- <div class="md:grid md:grid-cols-2 gap-x-12 mb-14">
+    
+   <div>
+      <h4 class="sm:!mt-0">Trial Membership</h4>
+      <p>
+        • Use of Practice range on day of play only<br/>
+        • Ten-day advance booking<br/>
+        • Up to six rounds per month for single membership<br/>
+        • Up to twelve rounds for family<br/>
+        • Must be purchased in advance<br/>
+        • Valid for 30 days from purchase date<br/>
+        • $85/ additional round (Homeowner)<br/>
+        • $95/ additional round (Non-Homeowner)<br/>
+        • $75 per month F&B Minimum for Non-Homeowner<br/>
+        • $75/month fee for personal cart trail use<br/>
+      </p>
+    </div> 
+
+
+   <div>
+      <h4 class="sm:!mt-0">Trial Membership Fees</h4>
+      <div class="grid grid-cols-2 gap-x-4">
+          <div>Homeowner Single</div>
+          <div class="font-bold">$450</div>
+          <div>Homeowner Family</div>
+          <div class="font-bold">$900</div>
+          <div>Non-Homeowner Single</div>
+          <div class="font-bold">$480</div>
+          <div>Non-Homeowner Family</div>
+          <div class="font-bold">$950</div>
+      </div>
+    </div> 
+
+  </div>
+
+  <hr/> -->
+
+  <!-- <div class="mb-14">
+    <h4 class="sm:!mt-0"></h4>
+    <div class="grid grid-cols-2 gap-x-4">
+      <div></div>
+      <div class="font-bold"></div>
+      <div></div>
+      <div class="font-bold"></div>
+    </div>
+  </div> -->
+
+  <!-- <div class="mb-14">
+    <h4 class="sm:!mt-0"> </h4>
+    <div class="grid grid-cols-2 gap-x-4">
+      <div></div>
+      <div class="font-bold"></div>
+    </div>
+  </div> -->
+
+  <div>
+    <h4 class="sm:!mt-0">Homeowner Details</h4>
+    <p>
+      • Use of practice range on day of play<br/>
+      • Fourteen-day advance booking<br/>
+      • Six rounds per year at $70 (Includes Guests with homeowner present)<br/>
+      • Additional rounds at 10% off best public rate for date & time
+    </p>
+  </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
